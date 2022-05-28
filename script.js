89ff4f62dec8f93dd6475952c318663cbb7dc956
Katechka <pinkgirl06@mail.ru>
2022-05-28 21:05:11 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+//Comment ctrl + slash
+
+/*Kiss
+    Me!*/
+// let answer = prompt("Есть 18 лет?", "Да");
+// console.log(typeof(answer));
+
+let incr = 10,
+    decr = 10;
+
+console.log(++incr);
+console.log(--decr);
